0805ca1ed50bf59840bc3b40047688755ea93d75
Mikhail Burmistrov <schlaf2003@mail.ru>
2023-03-23 02:31:47 +0300

    1

--- RoboCopy.ps1
+++ RoboCopy.ps1
@@ -1,8 +1,17 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2639
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+#Script is suitable to copy all files across  HDD\Servers with NTFS permissions saving.
 
-\f0\fs24 \cf0 robocopy \'93SourcePath:\\SourceFolder\'94 \'93\\\\Remote.Server\\d$\\DestinationFolder *.* /TEE /E /NP /V /DCOPY:T /COPY:DATSOU /PURGE /MIR /R:1 /W:1 /ZB /mt:16 /unilog:C:\\RoboCopyLogs\\FolderName_Migration-20230322-RobocopyResults.log}
+robocopy "D:\SourceFolder" "\\RemoteServer\d$\DestinationFolder" *.* /TEE /E /NP /V /DCOPY:T /COPY:DATSOU /PURGE /MIR /R:1 /W:1 /ZB /mt:16 /unilog:C:\MigrationReportPath\SourceFolder-20230322-RobocopyResults.log
+
+#Used keys:
+#/TEE
+#/E
+#/NP
+#/V
+#/DCOPY:T
+#/COPY:DATSOU
+#/PURGE
+#/MIR
+#/R:1
+#/W:1
+#/ZB
+#/mt:16
