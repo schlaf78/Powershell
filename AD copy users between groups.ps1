3433a5a68fc85d3920a6e53d922edde04edbcfce
Mikhail Burmistrov <schlaf2003@mail.ru>
2023-04-05 10:45:26 +0300

    Create AD copy users between groups.ps1

---
+++ AD copy users between groups.ps1
@@ -0,0 +1,10 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2639
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww21560\viewh10200\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 $gr = Get-ADGroupMember \'93SOURCE AD GROUP\'94\
+Add-ADGroupMember -Identity "CN=TARGET GROUP,OU=TARGET OU for the group Group,OU=GROUP OU LOCATION IN AD,DC=DOMAIN NAME,DC=DOMAIN,DC=ru" -Members $gr\
+}
